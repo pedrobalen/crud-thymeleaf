5cdc6a3ede53978d5cc6720981943f173ad2ce43
laboratorio <p.balen99@gmail.com>
2025-03-24 21:49:46 -0300

    crud completo

--- src/main/java/com/example/demo/controller/ProdutoController.java
+++ src/main/java/com/example/demo/controller/ProdutoController.java
@@ -1,16 +1,18 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Produto;
+import com.example.demo.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/produto")
 public class ProdutoController {
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
     @GetMapping("/novo")
     public String formNovo(Model model) {
         model.addAttribute("produto", new Produto());
@@ -18,7 +20,29 @@ public class ProdutoController {
     }
 
     @PostMapping
-    public void salvar(@ModelAttribute Produto produto) {
+    public String salvar(@ModelAttribute Produto produto) {
         System.out.println(produto.toString());
+        produtoRepository.save(produto);
+        return "redirect:/produto/novo";
+    }
+
+
+    @GetMapping
+    public String listar(Model model) {
+        model.addAttribute("produtos", produtoRepository.findAll());
+        return "lista";
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deletar(@PathVariable Long id) {
+        produtoRepository.deleteById(id);
+        return "redirect:/produto";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String formEditar(@PathVariable Long id, Model model) {
+        Produto produto = produtoRepository.findById(id).get();
+        model.addAttribute("produto", produto);
+        return "form";
     }
 }
